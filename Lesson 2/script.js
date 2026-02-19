4ef29b09c3dcd968169e450ca85107299557ed56
natalidimova00 <natalidimova00@abv.bg>
2026-02-19 20:09:12 +0200

    added Lesson 2 code

---
+++ Lesson 2/script.js
@@ -0,0 +1,7 @@
+const nextBtn = document.getElementById('btn-next');
+if (nextBtn) {
+    nextBtn.addEventListener('click', function() {
+        window.location.href = "snowboard.html";
+    });
+}
+
